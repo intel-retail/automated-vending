e631065b372df6e20f23a836be084a9112787ad6
Antonio Martinez <jose.antonio.martinez.torres@intel.com>
2021-05-01 16:39:52 -0700

    updating to openVINO 2021

--- ds-cv-inference/entrypoint.sh
+++ ds-cv-inference/entrypoint.sh
@@ -10,6 +10,6 @@ CONFIDENCE=$3
 SKUS=$4
 
 
-source /opt/intel/openvino/bin/setupvars.sh
+source /opt/intel/openvino_2021/bin/setupvars.sh
 
 /go/src/ds-cv-inference/ds-cv-inference -dir $DIR -mqtt $MQTT -skuMapping $SKUS -model /go/src/ds-cv-inference/product-detection-0001/FP32/product-detection-0001.bin -config /go/src/ds-cv-inference/product-detection-0001/FP32/product-detection-0001.xml -confidence $CONFIDENCE
--- ds-cv-inference/go.mod
+++ ds-cv-inference/go.mod
@@ -8,6 +8,6 @@ go 1.13
 require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/stretchr/testify v1.6.0
-	gocv.io/x/gocv v0.24.0
+	gocv.io/x/gocv v0.27.0
 	golang.org/x/net v0.0.0-20200513185701-a91f0712d120 // indirect
 )
