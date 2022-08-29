5bcb74d32d0be7275d6ce899775a4f072684e3d5
Valina Li <valina.li@intel.com>
2022-08-29 13:53:48 -0700

    feat: upgrade ms-inventory service to edgex 2.2
    Signed-off-by: Valina Li <valina.li@intel.com>

--- ms-inventory/main.go
+++ ms-inventory/main.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Intel Corporation. All rights reserved.
+// Copyright © 2022 Intel Corporation. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
 package main
@@ -6,10 +6,10 @@ package main
 import (
 	"ms-inventory/routes"
 
-	"fmt"
 	"os"
 
-	"github.com/edgexfoundry/app-functions-sdk-go/appsdk"
+	"github.com/edgexfoundry/app-functions-sdk-go/v2/pkg"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/logger"
 )
 
 const (
@@ -17,51 +17,45 @@ const (
 )
 
 func main() {
-	// Create an instance of the EdgeX SDK and initialize it.
-	edgexSdk := &appsdk.AppFunctionsSDK{ServiceKey: serviceKey}
-	if err := edgexSdk.Initialize(); err != nil {
-		edgexSdk.LoggingClient.Error(fmt.Sprintf("SDK initialization failed: %v\n", err))
-		os.Exit(-1)
+	// TODO: See https://docs.edgexfoundry.org/2.2/microservices/application/ApplicationServices/
+	//       for documentation on application services.
+	service, ok := pkg.NewAppService(serviceKey)
+	if !ok {
+		os.Exit(1)
 	}
-
-	// Retrieve the application settings from configuration.toml
-	appSettings := edgexSdk.ApplicationSettings()
-	if appSettings == nil {
-		edgexSdk.LoggingClient.Error("No application settings found")
-		os.Exit(-1)
-	}
+	lc := service.LoggingClient()
 
 	var err error
-	err = edgexSdk.AddRoute("/inventory", routes.InventoryGet, "GET")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/inventory", routes.InventoryGet, "GET")
+	errorAddRouteHandler(lc, err)
 
-	err = edgexSdk.AddRoute("/inventory", routes.InventoryPost, "POST", "OPTIONS")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/inventory", routes.InventoryPost, "POST", "OPTIONS")
+	errorAddRouteHandler(lc, err)
 
-	err = edgexSdk.AddRoute("/inventory/delta", routes.DeltaInventorySKUPost, "POST", "OPTIONS")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/inventory/delta", routes.DeltaInventorySKUPost, "POST", "OPTIONS")
+	errorAddRouteHandler(lc, err)
 
-	err = edgexSdk.AddRoute("/inventory/{sku}", routes.InventoryItemGet, "GET")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/inventory/{sku}", routes.InventoryItemGet, "GET")
+	errorAddRouteHandler(lc, err)
 
-	err = edgexSdk.AddRoute("/inventory/{sku}", routes.InventoryDelete, "DELETE", "OPTIONS")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/inventory/{sku}", routes.InventoryDelete, "DELETE", "OPTIONS")
+	errorAddRouteHandler(lc, err)
 
-	err = edgexSdk.AddRoute("/auditlog", routes.AuditLogGetAll, "GET", "OPTIONS")
-	errorAddRouteHandler(edgexSdk, err)
-	err = edgexSdk.AddRoute("/auditlog", routes.AuditLogPost, "POST")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/auditlog", routes.AuditLogGetAll, "GET", "OPTIONS")
+	errorAddRouteHandler(lc, err)
 
-	err = edgexSdk.AddRoute("/auditlog/{entry}", routes.AuditLogGetEntry, "GET", "OPTIONS")
-	errorAddRouteHandler(edgexSdk, err)
-	err = edgexSdk.AddRoute("/auditlog/{entry}", routes.AuditLogDelete, "DELETE")
-	errorAddRouteHandler(edgexSdk, err)
+	err = service.AddRoute("/auditlog", routes.AuditLogPost, "POST")
+	errorAddRouteHandler(lc, err)
 
-	// Tell the SDK to "start" and begin listening for events to trigger the pipeline
-	err = edgexSdk.MakeItRun()
-	if err != nil {
-		edgexSdk.LoggingClient.Error("MakeItRun returned error: ", err.Error())
-		os.Exit(-1)
+	err = service.AddRoute("/auditlog/{entry}", routes.AuditLogGetEntry, "GET", "OPTIONS")
+	errorAddRouteHandler(lc, err)
+
+	err = service.AddRoute("/auditlog/{entry}", routes.AuditLogDelete, "DELETE")
+	errorAddRouteHandler(lc, err)
+
+	if err := service.MakeItRun(); err != nil {
+		lc.Errorf("MakeItRun returned error: %s", err.Error())
+		os.Exit(1)
 	}
 
 	// Do any required cleanup here
@@ -69,9 +63,9 @@ func main() {
 	os.Exit(0)
 }
 
-func errorAddRouteHandler(edgexSdk *appsdk.AppFunctionsSDK, err error) {
+func errorAddRouteHandler(lc logger.LoggingClient, err error) {
 	if err != nil {
-		edgexSdk.LoggingClient.Error("Error adding route: %v", err.Error())
-		os.Exit(-1)
+		lc.Errorf("Error adding route: %s", err.Error())
+		os.Exit(1)
 	}
 }
