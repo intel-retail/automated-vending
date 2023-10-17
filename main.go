ed22cb47321a8703ea77d0bbea3da6f778f9391b
Brian McGinn <brian.mcginn@intel.com>
2023-10-17 11:09:46 -0700

    fix: Add go file for codeql
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

---
+++ main.go
@@ -0,0 +1,12 @@
+// Copyright © 2023 Intel Corporation. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("codeql")
+}
